b85b7cf80b1fc25484bbb130443cb16522eb3b55
Rubenscauan <98960560+Rubenscauan@users.noreply.github.com>
2022-07-22 01:05:21 -0300

    correção dos codigos, repositorio com2 princesas

--- Princesa/Puxando.cpp
+++ Princesa/Puxando.cpp
@@ -1,27 +1,71 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <list>
 using namespace std;
 
-void show_vet(std::vector<int> vet, int sword) {
-    for (int i = 0; i < (int) vet.size(); i++)
-        std::cout << vet[i] << (i == sword? "> " : "  ");
-    std::cout << std::endl;
+
+vector<int> PreencherVetor(int tamanho,vector<int> &vetor){
+    for(int i = 0; i < tamanho;i++){
+        vetor.push_back(i);
+    }
+    return vetor;
 }
 
-int main() {
-    int size {}, sword {};
-    std::cin >> size >> sword;
-    std::vector<int> vet(size);
-    for (int i = 0; i < size; i++)
-        vet[i] = i + 1; // preenche o vetor
-    sword -= 1;
-    while(vet.size() > 1) {
-        show_vet(vet, sword);
-        sword = (sword + 1) % vet.size();
-        vet.erase(vet.begin() + sword);
-        sword = sword % vet.size();
+void apagarPosicao(int posicao,vector<int> &vetor){
+    for(int i = posicao; i < vetor.size();i++){
+        vetor[posicao] = vetor[posicao+1];
+        posicao++;
     }
-    show_vet(vet, sword);
+    vetor.pop_back();
+}
+
+vector<bool> PreencherBool(int tamanho,vector<bool> & vetor){
+    for(int i = 0; i < tamanho;i++){
+        vetor.push_back(true);
+    }
+    return vetor;
+    
+}
+
+void printar(vector<int> vetor){
+    cout << "[";
+    for(int i = 0; i < vetor.size();i++){
+        if(i == vetor.size() - 1){
+            cout << vetor[i] << "]\n";
+            return;
+        }
+        cout << vetor[i] << ", ";
+    }
+}
+
+void princesa_puxando(int escolhido,vector<int> &vetor){
+    while(vetor.size() > 1){
+        vetor.erase(vetor.begin() + (escolhido + 1) % vetor.size());
+        escolhido = (escolhido + 1) % vetor.size();
+        printar(vetor);
+    }
+}
+
+void princesa_marcando(int escolhido,vector<int> &vetor){
+    vector<bool> aux;
+    PreencherBool(vetor.size(),aux);
+    for(int i = escolhido; vetor.size() > 1; i++){
+        if(aux[i+1] == true){
+            aux[i+1] = false;
+            apagarPosicao((escolhido+1)%vetor.size(),vetor);
+            
+            escolhido = (escolhido+1)%vetor.size();
+            printar(vetor);
+        }
+    }
+}
+
+
+int main()
+{
+    vector<int> vetor;
+    PreencherVetor(6,vetor);
+    princesa_puxando(0,vetor);
+    return 0;
+    
 }
